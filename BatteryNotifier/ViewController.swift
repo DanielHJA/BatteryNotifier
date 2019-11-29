efb795ec7a976a36ee2689fd311ef5b7191f1487
Daniel <daniel.hjartstrom@gmail.com>
2019-11-29 12:32:49 +0100

    Initial Commit

---
+++ BatteryNotifier/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BatteryNotifier
+//
+//  Created by Daniel Hjärtström on 2019-11-29.
+//  Copyright © 2019 Daniel Hjärtström. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
